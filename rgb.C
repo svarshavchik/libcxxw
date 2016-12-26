e55e9d747b7868658a50ea3da975d2063f2db72d
Sam Varshavchik <mrsam@courier-mta.com>
2016-12-26 18:58:38 -0500

    RGB class.

---
+++ rgb.C
@@ -0,0 +1,53 @@
+#include "libcxxw_config.h"
+#include "x/w/rgb.H"
+#include "messages.H"
+
+namespace LIBCXX_NAMESPACE {
+	namespace w {
+#if 0
+	}
+}
+#endif
+
+rgb rgb::gradient(const rgb &other, uint16_t i, uint16_t n) const
+{
+	if (n == 0) return other; // GIGO
+
+#define SCALE(channel) ((uint16_t)					\
+			(int32_t)channel +				\
+			(((int32_t)other.channel-channel)*(int32_t)i+(n/2))/n)
+
+	return rgb(SCALE(r), SCALE(g), SCALE(b), SCALE(a));
+}
+
+
+rgb rgb::gradient(const gradient_t &g, size_t i, size_t n)
+{
+	if (g.find(0) == g.end())
+		throw EXCEPTION(_("Color #0 must be specified for a gradient"));
+
+	auto last=--g.end();
+
+	auto k=last->first;
+
+	// First, map i to the range 0..k
+
+	uint16_t scaled_i= (i*k)/n;
+
+	auto next=g.upper_bound(scaled_i);
+
+	if (next == g.end())
+		return (--next)->second; // Edge condition.
+
+	auto prev=next;
+	--prev;
+
+	return prev->second.gradient(next->second, scaled_i-prev->first,
+				     next->first-prev->first);
+}
+#if 0
+{
+	{
+#endif
+	}
+}
