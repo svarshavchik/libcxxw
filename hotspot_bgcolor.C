25b1779339ae9f6ae8538fb6566ef7dc5af79297
Sam Varshavchik <mrsam@courier-mta.com>
2017-03-26 15:26:54 -0400

    Hotspot with background colors element mixin.

---
+++ hotspot_bgcolor.C
@@ -0,0 +1,19 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "hotspot_bgcolor.H"
+#include "hotspot.H"
+
+LIBCXXW_NAMESPACE_START
+
+hotspot_bgcolorObj::hotspot_bgcolorObj(const ref<implObj> &impl)
+	: hotspotObj(ref<hotspotObj::implObj>(&impl->get_hotspot_impl())),
+	  impl(impl)
+{
+}
+
+hotspot_bgcolorObj::~hotspot_bgcolorObj()=default;
+
+LIBCXXW_NAMESPACE_END
---
+++ hotspot_bgcolor_impl.C
@@ -0,0 +1,43 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "hotspot.H"
+#include "hotspot_bgcolor.H"
+#include "child_element.H"
+#include "background_color.H"
+
+LIBCXXW_NAMESPACE_START
+
+hotspot_bgcolorObj::implObj
+::implObj(const background_color &cold_color,
+	  const background_color &warm_color,
+	  const background_color &hot_color)
+	: cold_color_thread_only(cold_color),
+	  warm_color_thread_only(warm_color),
+	  hot_color_thread_only(hot_color)
+{
+}
+
+hotspot_bgcolorObj::implObj::~implObj()=default;
+
+void hotspot_bgcolorObj::implObj::temperature_changed(IN_THREAD_ONLY)
+{
+	auto &hotspot_impl=get_hotspot_impl();
+	auto &element=hotspot_impl.get_hotspot_element();
+
+	switch (hotspot_impl.hotspot_temperature(IN_THREAD)) {
+	case temperature::cold:
+		element.set_background_color(IN_THREAD, cold_color(IN_THREAD));
+		break;
+	case temperature::warm:
+		element.set_background_color(IN_THREAD, warm_color(IN_THREAD));
+		break;
+	case temperature::hot:
+		element.set_background_color(IN_THREAD, hot_color(IN_THREAD));
+		break;
+	}
+}
+
+LIBCXXW_NAMESPACE_END
