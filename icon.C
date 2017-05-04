bad5bebb796793934411adcf27cf27ee82f58054
Sam Varshavchik <mrsam@courier-mta.com>
2017-05-03 21:57:47 -0400

    Icons.
    Add create_icon_mm() to the drawable implementation object, to create
    an icon object.

---
+++ icon.C
@@ -0,0 +1,28 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "icon.H"
+#include "icon_image.H"
+
+LIBCXXW_NAMESPACE_START
+
+iconObj::iconObj(const const_icon_image &image)
+	: image_thread_only(image)
+{
+}
+
+iconObj::~iconObj()=default;
+
+icon iconObj::initialize(IN_THREAD_ONLY)
+{
+	return theme_updated(IN_THREAD);
+}
+
+icon iconObj::theme_updated(IN_THREAD_ONLY)
+{
+	return icon(this);
+}
+
+LIBCXXW_NAMESPACE_END
--- icon_cache.C
+++ icon_cache.C
@@ -11,6 +11,7 @@
 #include "screen.H"
 #include "pixmap.H"
 #include "drawable.H"
+#include "icon.H"
 #include "x/w/picture.H"
 #include <x/refptr_hash.H>
 #include <x/number_hash.H>
@@ -78,9 +79,50 @@ struct icon_cacheObj::sxg_image_cache_key_hash
 	}
 };
 
+//////////////////////////////////////////////////////////////////////////
+
+struct icon_cacheObj::mm_image_cache_key {
+
+	std::string      filename;
+	defaulttheme     theme;
+	const_pictformat image_pictformat;
+	render_repeat    repeat;
+	double           widthmm;
+	double           heightmm;
+
+	bool operator==(const mm_image_cache_key &o) const
+	{
+		return filename == o.filename &&
+			theme == o.theme &&
+			image_pictformat == o.image_pictformat &&
+			repeat == o.repeat &&
+			widthmm == o.widthmm &&
+			heightmm == o.heightmm;
+	}
+};
+
+struct icon_cacheObj::mm_image_cache_key_hash
+	: public std::hash<std::string>,
+	  public std::hash<defaulttheme>,
+	  public std::hash<const_pictformat> {
+
+	size_t operator()(const mm_image_cache_key &k) const
+	{
+		return (std::hash<defaulttheme>::operator()(k.theme) +
+			std::hash<const_pictformat>::operator()
+			(k.image_pictformat)) ^
+			(std::hash<std::string>::operator()(k.filename) +
+			 (size_t)k.repeat + (((size_t)k.widthmm) << 4)
+			 + (((size_t)k.heightmm) << 12));
+	}
+};
+
+//////////////////////////////////////////////////////////////////////////
+
 icon_cacheObj::icon_cacheObj()
 	: sxg_parser_cache{sxg_parser_cache_t::create()},
-	  sxg_image_cache(sxg_image_cache_t::create())
+	  sxg_image_cache(sxg_image_cache_t::create()),
+	  mm_image_cache(mm_image_cache_t::create())
 {
 }
 
@@ -105,6 +147,163 @@ sxg_parser get_sxg(const std::experimental::string_view &filename,
 		 {
 			 return sxg_parser::create(f, screenref, theme);
 		 });
+}
+
+///////////////////////////////////////////////////////////////////////
+
+//! An SXG-based icon.
+
+class LIBCXX_HIDDEN sxg_mmiconObj : public iconObj {
+
+	//! The parsed SXG object.
+	sxg_parser      current_sxg_thread_only;
+
+	//! The image created from the object
+
+	const_sxg_image current_image_thread_only;
+
+ public:
+
+	THREAD_DATA_ONLY(current_sxg);
+	THREAD_DATA_ONLY(current_image);
+
+	//! Name
+	const std::string name;
+
+	//! Original requested width in millimeters
+	const double widthmm;
+
+	//! Original requested height in millimeters
+	const double heightmm;
+
+	//! Constructor
+	sxg_mmiconObj(const std::experimental::string_view &name,
+		      double widthmm,
+		      double heightmm,
+		      const sxg_parser &orig_sxg,
+		      const const_sxg_image &orig_sxg_image);
+
+	//! Destructor
+	~sxg_mmiconObj();
+
+	//! Check if a new theme is installed, if so create a replacement icon.
+
+	icon theme_updated(IN_THREAD_ONLY) override;
+};
+
+//! Here's a parsed SXG image, the drawable it's for, and its dimensions.
+
+//! Calculate the actual size, in pixels, and return a rendered image from
+//! the SXG file. The returned sxg_images are cached.
+
+static inline const_sxg_image create_sxg_image(drawableObj::implObj
+					       *drawable_impl,
+					       const sxg_parser &sxg,
+					       render_repeat repeat,
+					       double widthmm,
+					       double heightmm)
+{
+	dim_t w=0;
+	dim_t h=0;
+
+	if (widthmm <= 0 && heightmm <= 0)
+	{
+		w=sxg->default_width();
+		h=sxg->default_height();
+	}
+	else
+	{
+		w=widthmm <= 0 ? 0 : sxg->width_for_mm(widthmm);
+		h=heightmm <= 0 ? 0 : sxg->height_for_mm(heightmm);
+
+		if (w == 0)
+			w=sxg->width_for_height(h, icon_scale::nearest);
+
+		if (h == 0)
+			h=sxg->height_for_width(w, icon_scale::nearest);
+	}
+
+	return sxg->screenref->impl->iconcaches
+		->sxg_image_cache->find_or_create
+		({drawable_impl->drawable_pictformat, repeat, w, h},
+		 [&]
+		 {
+			 auto pixmap=drawable_impl->create_pixmap(w, h);
+			 auto picture=pixmap->create_picture();
+
+			 auto ri=sxg_image::create(pixmap, picture, repeat);
+
+			 sxg->render(picture, pixmap, ri->points);
+
+			 return ri;
+		 });
+}
+
+icon drawableObj::implObj
+::create_icon_mm(const std::experimental::string_view &name,
+		 render_repeat icon_repeat,
+		 double widthmm,
+		 double heightmm)
+{
+	auto screen=get_screen();
+
+	auto theme=*current_theme_t::lock{screen->impl->current_theme};
+
+	return screen->impl->iconcaches->mm_image_cache->find_or_create
+		({std::string(name), theme,
+				drawable_pictformat,
+				icon_repeat, widthmm, heightmm},
+			[&, this]
+			{
+				auto sxg=get_sxg(name, screen, theme);
+
+				auto image=create_sxg_image(this, sxg,
+							    icon_repeat,
+							    widthmm, heightmm);
+
+				return ref<sxg_mmiconObj>::create(name,
+								  widthmm,
+								  heightmm,
+								  sxg, image);
+			});
+}
+
+sxg_mmiconObj::sxg_mmiconObj(const std::experimental::string_view &name,
+			     double widthmm,
+			     double heightmm,
+			     const sxg_parser &orig_sxg,
+			     const const_sxg_image &orig_sxg_image)
+		: iconObj(orig_sxg_image),
+		current_sxg_thread_only(orig_sxg),
+		current_image_thread_only(orig_sxg_image),
+		name(name),
+		widthmm(widthmm),
+		heightmm(heightmm)
+{
+}
+
+sxg_mmiconObj::~sxg_mmiconObj()=default;
+
+
+icon sxg_mmiconObj::theme_updated(IN_THREAD_ONLY)
+{
+	auto theme= *current_theme_t::lock{current_sxg(IN_THREAD)
+					   ->screenref->impl
+					   ->current_theme};
+
+	if (theme == current_sxg(IN_THREAD)->theme)
+		return icon(this); // Unchanged
+
+	// All right, take it from the top.
+	auto icon=image(IN_THREAD)->icon_pixmap->impl
+		->create_icon_mm(name,
+				 image(IN_THREAD)->repeat,
+				 widthmm,
+				 heightmm);
+
+	// We technically need to call initialize(), hopefully a mere
+	// formality.
+	return icon->initialize(IN_THREAD);
 }
 
 LIBCXXW_NAMESPACE_END
