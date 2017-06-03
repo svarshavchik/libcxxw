9cd7b0352991f5be823c4349f52babc18fc22917
Sam Varshavchik <mrsam@courier-mta.com>
2017-06-03 16:41:00 -0400

    A mixin for an element peepholed in the top level display element.

---
+++ peepholed_toplevel.C
@@ -0,0 +1,14 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "peepholed_toplevel.H"
+
+LIBCXXW_NAMESPACE_START
+
+peepholed_toplevelObj::peepholed_toplevelObj()=default;
+
+peepholed_toplevelObj::~peepholed_toplevelObj()=default;
+
+LIBCXXW_NAMESPACE_END
