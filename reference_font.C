2ec7881bc5e4321176aa6b8e23e3638f957fdcc7
Sam Varshavchik <mrsam@courier-mta.com>
2017-06-10 11:07:04 -0400

    The reference_font mixin.

---
+++ reference_font.C
@@ -0,0 +1,41 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "reference_font.H"
+#include "generic_window_handler.H"
+#include "fonts/current_fontcollection.H"
+#include "fonts/fontcollection.H"
+
+LIBCXXW_NAMESPACE_START
+
+reference_fontObj::reference_fontObj(elementObj::implObj &e,
+				     const std::experimental::string_view
+				     &fontname)
+	: reference_fontObj(e.get_window_handler().create_theme_font(fontname))
+{
+}
+
+reference_fontObj::reference_fontObj(const current_fontcollection &reference_font)
+	: reference_font_thread_only(reference_font)
+{
+}
+
+reference_fontObj::~reference_fontObj()=default;
+
+void reference_fontObj::initialize(IN_THREAD_ONLY)
+{
+	theme_updated(IN_THREAD);
+}
+
+void reference_fontObj::theme_updated(IN_THREAD_ONLY)
+{
+	reference_font(IN_THREAD)->theme_updated(IN_THREAD);
+	font_nominal_width(IN_THREAD)=
+		reference_font(IN_THREAD)->fc(IN_THREAD)->nominal_width();
+	font_height(IN_THREAD)=
+		reference_font(IN_THREAD)->fc(IN_THREAD)->height();
+}
+
+LIBCXXW_NAMESPACE_END
