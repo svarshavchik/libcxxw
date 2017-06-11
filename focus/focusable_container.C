818140d4df8957f4188a0005a0d467b3b5750be6
Sam Varshavchik <mrsam@courier-mta.com>
2017-06-11 17:07:34 -0400

    Framework for focusable containers.

---
+++ focus/focusable_container.C
@@ -0,0 +1,12 @@
+/*
+** Copyright 2017 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "x/w//focusable_container.H"
+
+LIBCXXW_NAMESPACE_START
+
+focusable_containerObj::~focusable_containerObj()=default;
+
+LIBCXXW_NAMESPACE_END
