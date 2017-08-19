b6368878729ce4702a1538dfeb39985932ff3cf9
Sam Varshavchik <mrsam@courier-mta.com>
2017-08-19 09:58:18 -0400

    Factor out label creation.

--- textlabel_impl.C
+++ textlabel_impl.C
@@ -31,14 +31,23 @@ label factoryObj::create_label(const text_param &text,
 			       double widthmm,
 			       halign alignment)
 {
-	auto label_impl=ref<label_elementObj<child_elementObj>>
-		::create(container_impl, text, alignment, widthmm);
-
-	auto l=label::create(label_impl, label_impl);
+	auto l=create_label_internal(text, widthmm, alignment, container_impl);
 
 	created(l);
 
 	return l;
+}
+
+label factoryObj::create_label_internal(const text_param &text,
+					double widthmm,
+					halign alignment,
+					const ref<containerObj::implObj>
+					&container_impl)
+{
+	auto label_impl=ref<label_elementObj<child_elementObj>>
+		::create(container_impl, text, alignment, widthmm);
+
+	return label::create(label_impl, label_impl);
 }
 
 textlabelObj::implObj::implObj(const text_param &text,
