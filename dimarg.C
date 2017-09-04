38bdf78a6f80e59ea04f4aafb7773ffdfa274cfc
Sam Varshavchik <mrsam@courier-mta.com>
2017-09-04 10:42:22 -0400

    Reimplement dimarg using std::variant

--- dimarg.C
+++ dimarg.C
@@ -4,19 +4,36 @@
 */
 #include "libcxxw_config.h"
 #include "x/w/dimarg.H"
+#include <x/visitor.H>
 #include <sstream>
 
 LIBCXXW_NAMESPACE_START
 
+dimarg::dimarg(const std::string_view &string_arg)
+	: superclass_t{string_arg}
+{
+}
+
+dimarg::dimarg(double v) : superclass_t{v}
+{
+}
+
 dimarg::operator std::string() const
 {
-	if (string_arg)
-		return std::string{*string_arg};
+	return std::visit(visitor{
+			[](const std::string_view &s) {
+				return std::string{s};
+			},
+			[](const double &d)
+			{
+				std::ostringstream o;
 
-	std::ostringstream o;
-
-	o << double_arg;
-	return o.str();
+				o << d;
+				return o.str();
+			}
+		}, static_cast<const superclass_t>(*this));
 }
+
+dimarg::~dimarg()=default;
 
 LIBCXXW_NAMESPACE_END
