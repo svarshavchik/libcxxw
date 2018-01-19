168a50b72f9c3d14090d2805fa1a8ff79ede3170
Sam Varshavchik <mrsam@courier-mta.com>
2018-01-18 19:53:47 -0500

    Factor out peepholed_fontelementObj.
    Template mixin subclass of peepholed_elementObj that implements the
    increment functions using a reference font.

--- editor.C
+++ editor.C
@@ -6,12 +6,12 @@
 #include "editor.H"
 #include "editor_impl.H"
 #include "x/w/factory.H"
-#include "peephole/peepholed_element.H"
+#include "peephole/peepholed_fontelement.H"
 
 LIBCXXW_NAMESPACE_START
 
 editorObj::editorObj(const ref<implObj> &impl)
-	: superclass_t(impl),
+	: superclass_t{impl, impl},
 	  impl(impl)
 {
 }
@@ -22,16 +22,5 @@ focusable_impl editorObj::get_impl() const
 {
 	return impl;
 }
-
-dim_t editorObj::horizontal_increment(IN_THREAD_ONLY) const
-{
-	return impl->font_nominal_width(IN_THREAD);
-}
-
-dim_t editorObj::vertical_increment(IN_THREAD_ONLY) const
-{
-	return impl->font_height(IN_THREAD);
-}
-
 
 LIBCXXW_NAMESPACE_END
