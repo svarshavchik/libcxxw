7023f785e9340b7f8ccf02ba44449d437b252aa2
Sam Varshavchik <mrsam@courier-mta.com>
2019-02-10 22:22:17 -0500

    Remove popup_attachedto_handler_elementObj mixin.

--- input_field/input_field_search.C
+++ input_field/input_field_search.C
@@ -6,15 +6,24 @@
 #include "input_field/input_field_search.H"
 #include "input_field/input_field_search_popup_handler.H"
 #include "popup/popup.H"
-#include "popup/popup_attachedto_handler_element.H"
 
 LIBCXXW_NAMESPACE_START
+
+static inline child_element_init_params
+create_init_params(const popup &my_popup)
+{
+	child_element_init_params init_params;
+
+	init_params.attached_popup=my_popup;
+
+	return init_params;
+}
 
 input_field_searchObj
 ::input_field_searchObj(const popup &my_popup,
 			const ref<input_field_search_popup_handlerObj> &handler,
 			const container_impl &parent)
-	: superclass_t{handler, parent},
+	: superclass_t{parent, create_init_params(my_popup)},
 	  my_popup{my_popup},
 	  popup_handler{handler}
 {
