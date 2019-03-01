8e0d749b87e3fa5b50d67b4253ca9715f6a3d572
Sam Varshavchik <mrsam@courier-mta.com>
2019-02-28 19:04:48 -0500

    Framework for factoring out override-redirect logic.
    Set things up for moving most of popupObj::handlerObj that deals with
    override-redirect logic, leaving in place just the logic that implements
    override redirect based on an attached element's position.

---
+++ override_redirect.C
@@ -0,0 +1,14 @@
+/*
+** Copyright 2018-2019 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+#include "libcxxw_config.h"
+#include "override_redirect.H"
+
+LIBCXXW_NAMESPACE_START
+
+override_redirectObj::override_redirectObj()=default;
+
+override_redirectObj::~override_redirectObj()=default;
+
+LIBCXXW_NAMESPACE_END
