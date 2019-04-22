ed945a07ebb6e6b93275ec470650e774a323f1ea
Sam Varshavchik <mrsam@courier-mta.com>
2019-04-21 22:10:44 -0400

    Move load_colors into uicompiler.
    Have uicompiler handle loading of colors from a theme file. Moves the colors
    map into uigenerator.

--- uicompiler.C
+++ uicompiler.C
@@ -15,19 +15,465 @@
 #include "x/w/shortcut.H"
 #include "theme_parser_lock.H"
 #include "messages.H"
+#include "picture.H"
 #include <x/functionalrefptr.H>
 
 LIBCXXW_NAMESPACE_START
 
-uicompiler::uicompiler(const theme_parser_lock &lock,
+
+const char * const rgb_channels[]={"r",
+				   "g",
+				   "b",
+				   "a"};
+
+rgb_component_t rgb::* const rgb_fields[]=
+	{
+	 &rgb::r,
+	 &rgb::g,
+	 &rgb::b,
+	 &rgb::a};
+
+
+
+static inline bool scale_theme_color(theme_parser_lock &lock,
+				     const std::string &id,
+				     const std::string &scale,
+				     rgb &color,
+				     std::unordered_map<std::string,
+				     theme_color_t> &parsed_colors)
+{
+	std::istringstream s;
+
+	imbue<std::istringstream> imbue{lock.c_locale, s};
+
+	for (size_t i=0; i<4; ++i)
+	{
+		auto attribute=lock.clone();
+
+		auto xpath=attribute->get_xpath(rgb_channels[i]);
+
+		if (xpath->count() == 0)
+			continue;
+
+		xpath->to_node();
+
+		s.seekg(0);
+		s.str(attribute->get_text());
+
+		double v;
+
+		s >> v;
+
+		if (s.fail())
+			throw EXCEPTION(gettextmsg(_("could not parse color id=%1%"),
+						   id));
+
+		if (v < 0)
+			throw EXCEPTION(gettextmsg(_("negative color value for id=%1%"),
+						   id));
+		rgb_component_t c;
+
+		if (scale.empty())
+		{
+			if (v > 1)
+				v=1;
+			c=v * rgb::maximum;
+		}
+		else
+		{
+			v *= color.*(rgb_fields[i]);
+
+			if (v > rgb::maximum)
+				v=rgb::maximum;
+			c=v;
+		}
+
+		color.*(rgb_fields[i])=c;
+	}
+
+	return true;
+}
+
+static bool parse_gradients(theme_parser_lock &lock,
+			    rgb_gradient &gradient,
+			    std::unordered_map<std::string,
+			    theme_color_t> &parsed_colors,
+			    const std::string &id);
+
+static inline bool scale_theme_color(theme_parser_lock &lock,
+				     const std::string &id,
+				     const std::string &scale,
+				     linear_gradient &color,
+				     std::unordered_map<std::string,
+				     theme_color_t> &parsed_colors)
+{
+	static const char * const coords[]={"x1",
+					    "y1",
+					    "x2",
+					    "y2",
+					    "widthmm",
+					    "heightmm"};
+
+	static const double minvalue[]={0,0,0,0,-999,-999};
+	static const double maxvalue[]={1,1,1,1,999,999};
+
+	static double linear_gradient::* const fields[]={
+		&linear_gradient::x1,
+		&linear_gradient::y1,
+		&linear_gradient::x2,
+		&linear_gradient::y2,
+		&linear_gradient::fixed_width,
+		&linear_gradient::fixed_height};
+
+	std::istringstream s(lock->get_text());
+
+	imbue<std::istringstream> imbue{lock.c_locale, s};
+
+	for (size_t i=0; i<6; i++)
+	{
+		auto attribute=lock.clone();
+
+		auto xpath=attribute->get_xpath(coords[i]);
+
+		if (xpath->count() == 0)
+			continue;
+
+		xpath->to_node();
+
+		s.seekg(0);
+		s.str(attribute->get_text());
+
+		double v;
+
+		s >> v;
+
+		if (s.fail())
+			throw EXCEPTION(gettextmsg
+					(_("could not parse %2% for id=%1%"),
+					 id, coords[i]));
+
+		if (v < minvalue[i] || v>maxvalue[i])
+			throw EXCEPTION(gettextmsg
+					(_("%2% for id=%1% must be between %3%"
+					   " and %4%"),
+					 id, coords[i],
+					 minvalue[i], maxvalue[i]));
+
+		color.*(fields[i])=v;
+	}
+
+	bool flag;
+
+	try {
+		flag=parse_gradients(lock, color.gradient, parsed_colors, id);
+	} catch (const exception &e) {
+		std::ostringstream o;
+
+		o << e;
+
+		throw EXCEPTION(gettextmsg
+				(_("gradient id=%1% is not valid: %2%"),
+				 id, o.str()));
+	}
+
+	return flag;
+}
+
+static inline bool scale_theme_color(theme_parser_lock &lock,
+				     const std::string &id,
+				     const std::string &scale,
+				     radial_gradient &color,
+				     std::unordered_map<std::string,
+				     theme_color_t> &parsed_colors)
+{
+	static const char * const coords[]={"inner_x",
+					    "inner_y",
+					    "outer_x",
+					    "outer_y",
+					    "inner_radius",
+					    "outer_radius",
+					    "widthmm",
+					    "heightmm"};
+
+	static const double minvalue[]={0,0,0,0,0,0,-999,-999};
+	static const double maxvalue[]={1,1,1,1,999,999,999,999};
+
+	static double radial_gradient::* const fields[]={
+		&radial_gradient::inner_center_x,
+		&radial_gradient::inner_center_y,
+		&radial_gradient::outer_center_x,
+		&radial_gradient::outer_center_y,
+		&radial_gradient::inner_radius,
+		&radial_gradient::outer_radius,
+		&radial_gradient::fixed_width,
+		&radial_gradient::fixed_height};
+
+	std::istringstream s;
+
+	imbue<std::istringstream> imbue{lock.c_locale, s};
+
+	for (size_t i=0; i<6; i++)
+	{
+		auto attribute=lock.clone();
+
+		auto xpath=attribute->get_xpath(coords[i]);
+
+		if (xpath->count() == 0)
+			continue;
+
+		xpath->to_node();
+
+		s.seekg(0);
+		s.str(attribute->get_text());
+
+		double v;
+
+		s >> v;
+
+		if (s.fail())
+			throw EXCEPTION(gettextmsg
+					(_("could not parse %2% for id=%1%"),
+					 id, coords[i]));
+
+		if (v < minvalue[i] || v>maxvalue[i])
+			throw EXCEPTION(gettextmsg
+					(_("%2% for id=%1% must be between %3%"
+					   " and %4%"),
+					 id, coords[i],
+					 minvalue[i], maxvalue[i]));
+
+		color.*(fields[i])=v;
+	}
+
+	static const char * const raxises[]={"inner_radius_axis",
+					     "outer_radius_axis"};
+
+	static radial_gradient::radius_axis radial_gradient::* const rfields[]={
+		&radial_gradient::inner_radius_axis,
+		&radial_gradient::outer_radius_axis};
+
+	for (size_t i=0; i<2; i++)
+	{
+		if (!single_value_exists(lock, raxises[i]))
+			continue;
+
+		auto s=lowercase_single_value(lock, raxises[i], "color");
+
+		if (s.empty())
+			continue;
+
+		if (s == "horizontal")
+		{
+			color.*(rfields[i])=radial_gradient::horizontal;
+		}
+		else if (s == "vertical")
+		{
+			color.*(rfields[i])=radial_gradient::vertical;
+		}
+		else if (s == "shortest")
+		{
+			color.*(rfields[i])=radial_gradient::shortest;
+		}
+		else if (s == "longest")
+		{
+			color.*(rfields[i])=radial_gradient::longest;
+		}
+		else
+			throw EXCEPTION(gettextmsg
+					(_("%1% is not a valid value for %2%"
+					   ", id=%3%"),
+					 s, raxises[i], id));
+	}
+
+	bool flag;
+
+	try {
+		flag=parse_gradients(lock, color.gradient, parsed_colors, id);
+	} catch (const exception &e) {
+		std::ostringstream o;
+
+		o << e;
+
+		throw EXCEPTION(gettextmsg
+				(_("gradient id=%1% is not valid: %2%"),
+				 id, o.str()));
+	}
+
+	return flag;
+}
+
+static bool parse_gradients(theme_parser_lock &lock,
+			    rgb_gradient &parsed_gradient,
+			    std::unordered_map<std::string,
+			    theme_color_t> &parsed_colors,
+			    const std::string &id)
+{
+	auto gradients=lock.clone();
+
+	auto xpath=gradients->get_xpath("gradient");
+
+	size_t n=xpath->count();
+
+	std::istringstream s;
+
+	imbue<std::istringstream> imbue{lock.c_locale, s};
+
+	for (size_t i=0; i<n; ++i)
+	{
+		xpath->to_node(i+1);
+
+		auto gradient=gradients.clone();
+
+		auto vxpath=gradient->get_xpath("value");
+
+		vxpath->to_node();
+
+		unsigned n;
+
+		s.seekg(0);
+		s.str(gradient->get_text());
+
+		s >> n;
+
+		if (s.fail())
+			throw EXCEPTION(gettextmsg
+					(_("could not parse <value>"
+					   " for id=%1%"),
+					 id));
+
+		gradient=gradients.clone();
+		vxpath=gradient->get_xpath("color");
+		vxpath->to_node();
+
+		auto s=gradient->get_text();
+
+		if (s.empty())
+			continue;
+
+		auto iter=parsed_colors.find(s);
+
+		if (iter == parsed_colors.end())
+			return false; // Not yet parsed
+
+		if (!std::holds_alternative<rgb>(iter->second))
+			throw EXCEPTION(gettextmsg
+					(_("gradient id=%1% cannot use color "
+					   "%2"), id, s));
+		parsed_gradient.emplace(n, std::get<rgb>(iter->second));
+	}
+
+	valid_gradient(parsed_gradient);
+	return true;
+}
+
+uicompiler::uicompiler(const theme_parser_lock &root_lock,
 		       uigeneratorsObj &generators)
 	: generators{generators}
 {
-	auto xpath=lock->get_xpath("/theme/layout | /theme/factory");
+	if (!root_lock->get_root())
+		return;
+
+	auto lock=root_lock.clone();
+
+	auto xpath=lock->get_xpath("/theme/color");
+
+	size_t count=xpath->count();
+
+	bool parsed;
+
+	// Repeatedly pass over all colors, parsing the ones that are not based
+	// on unparsed colors.
+
+	do
+	{
+		parsed=false;
+
+		for (size_t i=0; i<count; ++i)
+		{
+			xpath->to_node(i+1);
+
+			auto id=lock->get_any_attribute("id");
+
+			if (id.empty())
+				throw EXCEPTION(_("no id specified for color"));
+
+			if (generators.colors.find(id) !=
+			    generators.colors.end())
+				continue; // Did this one already.
+
+			auto scale=lock->get_any_attribute("scale");
+
+			theme_color_t new_color;
+
+			if (!scale.empty())
+			{
+				auto iter=generators.colors.find(scale);
+
+				if (iter == generators.colors.end())
+					continue; // Not yet.
+
+				new_color=iter->second;
+			}
+			else
+			{
+				auto type=lock->get_any_attribute("type");
+
+				if (type.empty()) type="rgb";
+
+				if (type != "rgb" && type != "linear_gradient"
+				    && type != "radial_gradient")
+					throw EXCEPTION
+						(gettextmsg
+						 (_("Unknown type=%1% specified"
+						    " for id=%2%"),
+						  type, id));
+				if (type == "linear_gradient")
+					new_color=linear_gradient{};
+				if (type == "radial_gradient")
+					new_color=radial_gradient{};
+			}
+
+			bool flag=std::visit
+				([&]
+				 (auto &c)
+				 {
+					 return scale_theme_color
+						 (lock, id,
+						  scale, c,
+						  generators.colors);
+				 }, new_color);
+
+			if (flag)
+			{
+				generators.colors.insert({id, new_color});
+				parsed=true;
+			}
+
+		}
+	} while (parsed);
+
+	for (size_t i=0; i<count; ++i)
+	{
+		xpath->to_node(i+1);
+
+		auto id=lock->get_any_attribute("id");
+
+		if (generators.colors.find(id) != generators.colors.end())
+			continue;
+
+		throw EXCEPTION(gettextmsg
+				(_("Color %1% is based on another"
+				   " color which was not found"
+				   " (this can be because of a circular"
+				   " reference)"),
+				 id));
+	}
+
+	xpath=lock->get_xpath("/theme/layout | /theme/factory");
 
 	// Build the list of uncompiled_elements, by id.
 
-	size_t count=xpath->count();
+	count=xpath->count();
 
 	for (size_t i=0; i<count; ++i)
 	{
